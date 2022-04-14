c2ed6dc6611065273fa188951b4c4aaf2fe52c56
Anatolii Platonov <platonoff.dev@gmail.com>
2022-04-15 01:33:10 +0400

    Add solution for floyd

--- floyd/floyd.go
+++ floyd/floyd.go
@@ -2,5 +2,16 @@ package floyd
 
 // Triangle makes a Floyd's triangle matrix with rows count.
 func Triangle(rows int) [][]int {
-	return [][]int{}
+	result := make([][]int, rows)
+
+	current := 0
+	for i := 0; i < rows; i++ {
+		result[i] = make([]int, i+1)
+		for j := 0; j < i+1; j++ {
+			current++
+			result[i][j] = current
+		}
+	}
+
+	return result
 }
