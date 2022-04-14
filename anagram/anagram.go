4c1c53e033fb54493a7a3a2274d4c164b0036781
Anatolii Platonov <platonoff.dev@gmail.com>
2022-04-14 23:05:49 +0400

    Add solution for anagram

--- anagram/anagram.go
+++ anagram/anagram.go
@@ -1,5 +1,46 @@
 package anagram
 
+import "strings"
+
+func isAnagram(word1, word2 string) bool {
+	// Prepare strings for comparison
+	word1 = strings.ToLower(strings.Replace(word1, " ", "", -1))
+	word2 = strings.ToLower(strings.Replace(word2, " ", "", -1))
+
+	// Check basic cases when strings is not anagrams
+	if len(word1) != len(word2) || word1 == word2 {
+		return false
+	}
+
+	// Count letters in both string. Strings are anagrams if final count for each letter is 0
+	letterCounts := map[rune]int{}
+	for _, ch := range word1 {
+		letterCounts[ch] += 1
+	}
+
+	for _, ch := range word2 {
+		if _, ok := letterCounts[ch]; !ok {
+			return false
+		} else {
+			letterCounts[ch] -= 1
+		}
+	}
+
+	for _, count := range letterCounts {
+		if count != 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func FindAnagrams(dictionary []string, word string) (result []string) {
-	return []string{}
+	for _, dictWord := range dictionary {
+		if isAnagram(word, dictWord) {
+			result = append(result, dictWord)
+		}
+	}
+
+	return
 }
