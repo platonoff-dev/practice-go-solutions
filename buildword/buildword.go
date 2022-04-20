bdb14bd378a272007cee4f96981d14fb85aba4ed
Anatolii Platonov <platonoff.dev@gmail.com>
2022-04-20 23:14:49 +0400

    Add solution for buildword task

--- buildword/buildword.go
+++ buildword/buildword.go
@@ -1,5 +1,31 @@
 package buildword
 
+func SearchParts(word string, fragments []string) int {
+	minLength := 0
+	length := 0
+	for _, fragment := range fragments {
+		if len(fragment) > len(word) || word[:len(fragment)] != fragment {
+			continue
+		}
+
+		if fragment == word {
+			return 1
+		}
+
+		restLength := SearchParts(word[len(fragment):], fragments)
+		if restLength == 0 {
+			continue
+		}
+
+		length = restLength + 1
+		if length != 0 && (length < minLength || minLength == 0) {
+			minLength = length
+		}
+	}
+
+	return minLength
+}
+
 func BuildWord(word string, fragments []string) int {
-	return 0
+	return SearchParts(word, fragments)
 }
